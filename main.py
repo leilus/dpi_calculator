bdd88db70a17c621000b91ee7803af03380693f7
Leila Woźniak <105746710+leilus@users.noreply.github.com>
2023-10-05 18:58:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
 root = Tk()
 root.title("DPI Calculator")
@@ -8,7 +10,6 @@ label = Label(root, text="E-DPI Calculator", font="30", fg="white", bg="gray")
 label.pack(padx=5, pady=5)
 
 current_function = None
-
 def first_chose():
     clear_widgets()
     val_text = Label(root, text="Your valorant sens", font=20, bg="gray")
@@ -35,12 +36,56 @@ def first_chose():
         dpi_num = int(dpi_entry.get())
         result.config(text=round((sens_num * dpi_num), 1))
 
+
 def second_chose():
     clear_widgets()
-    sens_choice = Label(root, text="siema")
-    sens_choice.pack()
-    reset_button = Button(root, text="Go back", command=reset)
-    reset_button.pack()
+
+    def on_select(e):
+        csNum = float(3.18)
+        forNum = float(12.6)
+        selected_option = combo_var.get()
+        second_selected_option = combo_varSecond.get()
+        if (selected_option == "Valorant" and second_selected_option == "CS-GO"):
+            print("esa")
+
+    #option menu
+    from_text = Label(root, text="Convert from: ", bg="grey")
+    combo_var = tk.StringVar()
+    combo = ttk.Combobox(root, textvariable=combo_var)
+    combo['values'] = ('Valorant', 'CS-GO', 'Fortnite')
+    from_text.pack()
+    combo.pack()
+
+    to_text = Label(root, text="Convert to: ", bg="grey")
+    combo_varSecond = tk.StringVar()
+    comboSecond = ttk.Combobox(root, textvariable=combo_varSecond)
+    comboSecond['values'] = ('Valorant', 'CS-GO', 'Fortnite')
+    to_text.pack()
+    comboSecond.pack()
+
+    combo.bind('<<ComboboxSelected>>', on_select)
+    comboSecond.bind('<<ComboboxSelected>>', on_select)
+    sens_text = Label(root, text="Your sens", bg="grey")
+    your_sens = Entry(root, width=20)
+
+    dpi_text = Label(root, text="Your DPI", bg="grey")
+    your_dpi = Entry(root, width=20)
+
+    sens_text.pack()
+    your_sens.pack()
+    dpi_text.pack()
+    your_dpi.pack()
+
+    def cs_convert():
+        sens_num = float(your_sens.get())
+        dpi_num = int(your_dpi.get())
+        result.config(text=(round(sens_num * dpi_num)))
+
+    result = Label(root, text="", bg="grey")
+    result.pack()
+
+    conv_result = Button(root, text="Convert", command=cs_convert)
+    conv_result.pack()
 
 def reset():
     clear_widgets()
